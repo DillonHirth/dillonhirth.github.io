901913d564d9684777cd74be292b8cc302dae0af
dillon <dhirth.it@gmail.com>
2022-12-05 15:19:07 -0600

    cleaning it up, just a short sweet proto

--- JSToying/ApiRequests/index.js
+++ JSToying/ApiRequests/index.js
@@ -1,8 +1,6 @@
 const input = document.querySelector("#searchInput")
+const movieCards = document.querySelector("#movieCards")
 input.addEventListener('keyup', async function (e) {
-    // console.log(e)
-    // console.log(input.value)
-    //https://api.themoviedb.org/3/search/movie?api_key=68407b9912a8a1e274ab6c168666626f&language=en-US&query=galaxy&page=1&include_adult=false
     let url = `https://api.themoviedb.org/3/search/movie?api_key=${config.MY_KEY}&language=en-US&query=${input.value}&page=1&include_adult=false`
     console.log(url)
     const res = await axios.get(url)
@@ -11,9 +9,17 @@ input.addEventListener('keyup', async function (e) {
 })
 
 const makeImages = (movies) => {
+    //remove any existing images
+    movieCards.innerHTML = ""
     for (let item of movies) {
-        const img = document.createElement('IMG');
+        const div = document.createElement('div');
+        div.classList.add('movieCard', 'col-1')
+        const img = document.createElement('img');
+        img.classList.add('card')
         img.src = `https://www.themoviedb.org/t/p/w600_and_h900_bestv2/${item.poster_path}`
-        document.body.append(img)
+        const title = document.createElement('p')
+        title.innerText = item.title
+        div.append(img, title)
+        movieCards.appendChild(div)
     }
 }
